1ff72811f0d306c17a321d49de3536c6e463865a
kushal dave <kushaldave2011@gmail.com>
2019-10-09 01:05:25 +0530

    Initial Commit

---
+++ SwiftUi with combine demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUi with combine demo
+//
+//  Created by kushal dave on 09/10/19.
+//  Copyright © 2019 kushal dave. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
